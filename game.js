906691b667a8b39c3535a6ba3e501ea855c2d6cb
MikeHill89 <mvreugdenhil18@gmail.com>
2023-01-07 01:35:58 +0200

    Fix: Prettified file for readability

--- game.js
+++ game.js
@@ -1,97 +1,96 @@
 let gameScore = 5; // sets the amount of games by default to 5
-let playerScore = 0; // variable that can store the points when a player wins a round. 
+let playerScore = 0; // variable that can store the points when a player wins a round.
 let computerScore = 0; // varaible that can store the points when the computer wins a round.
 
 // function that can be called to show who's the winner of rock paper scissors
-function showWinner(){
-	if (playerScore > computerScore){
-		console.log(`You won!`);
-	} else if (playerScore === computerScore){
-		console.log(`Playing the addtional bonus round to determine the winner!`);
-		playRound();
-	} else {
-		console.log(`You lost!`);
-	}
+function showWinner() {
+  if (playerScore > computerScore) {
+    console.log(`You won!`);
+  } else if (playerScore === computerScore) {
+    console.log(`Playing the addtional bonus round to determine the winner!`);
+    playRound();
+  } else {
+    console.log(`You lost!`);
+  }
 }
 
 // functions generates a random number between 0 and 2 including 0 and 2. Then selects that number from the array and returns it.
-function computerSelection(){
-	const weapons = ["rock","paper","scissors"];
-	const randomWeapon = Math.floor(Math.random() * (weapons.length));//generates a randum number between 0 and 2
-	const pickedWeapon = weapons[randomWeapon]; //selects a randomWeapon from the array using the number as position 
-	return pickedWeapon; //returns the pickedWeapon as value
+function computerSelection() {
+  const weapons = ["rock", "paper", "scissors"];
+  const randomWeapon = Math.floor(Math.random() * weapons.length); //generates a randum number between 0 and 2
+  const pickedWeapon = weapons[randomWeapon]; //selects a randomWeapon from the array using the number as position
+  return pickedWeapon; //returns the pickedWeapon as value
 }
 
 // prompts the user for an input and then validates the input to be rock,paper or scissors. If the input is valid returns it value.
-function playerInput(){
-	let input = prompt('Rock, Paper or Scissors?');
-	input = input.toLowerCase();
+function playerInput() {
+  let input = prompt("Rock, Paper or Scissors?");
+  input = input.toLowerCase();
 
-	if (input === 'rock' || input === 'paper' || input === 'scissors'){
-		return input;
-	} else if (input === "" || input === null){
-		console.log('Can not be blank. Please type Rock,Paper or Scissors');
-		return playerInput();
-	} else{
-		console.log("Sorry, you need to write rock, paper or scissors");
-		return playerInput();
-	}
+  if (input === "rock" || input === "paper" || input === "scissors") {
+    return input;
+  } else if (input === "" || input === null) {
+    console.log("Can not be blank. Please type Rock,Paper or Scissors");
+    return playerInput();
+  } else {
+    console.log("Sorry, you need to write rock, paper or scissors");
+    return playerInput();
+  }
 }
-
 
 //usfd in playRound() to console log the score after a round is played
-function showScore(){
-	const scoreMessage = `Player has ${playerScore}. Computer score = ${computerScore}`;
-	console.log(scoreMessage);
+function showScore() {
+  const scoreMessage = `Player has ${playerScore}. Computer score = ${computerScore}`;
+  console.log(scoreMessage);
 }
 // function that can be called to reset the player, computer to 0 and game score to 5. Log's a message and plays another round.
-function resetGame(){
-	gameScore = 5;
-	playerScore = 0;
-	computerScore = 0;
-	console.info("Let's go again.");
-	playRound();
+function resetGame() {
+  gameScore = 5;
+  playerScore = 0;
+  computerScore = 0;
+  console.info("Let's go again.");
+  playRound();
 }
 
 //used in playRound to display the amount of games left. If 5 games are played shows the winner and calls the resetGame function
-function showRoundsLeft(){
-	if (gameScore === 1){
-		const showLastRound = 'Last round left!';
-		console.warn(showLastRound);
-		playRound();
-	} else if (gameScore > 1){
-		const showRoundsLeft = `${gameScore} round left.`;
-		console.info(showRoundsLeft);
-		playRound();
-	} else if (gameScore === 0 && (playerScore === computerScore)) {
-			console.warn('An extra round needs to be played to determine the winner');
-			showWinner();
-		}else {
-		showWinner();
-		resetGame();
-	}
+function showRoundsLeft() {
+  if (gameScore === 1) {
+    const showLastRound = "Last round left!";
+    console.warn(showLastRound);
+    playRound();
+  } else if (gameScore > 1) {
+    const showRoundsLeft = `${gameScore} round left.`;
+    console.info(showRoundsLeft);
+    playRound();
+  } else if (gameScore === 0 && playerScore === computerScore) {
+    console.warn("An extra round needs to be played to determine the winner");
+    showWinner();
+  } else {
+    showWinner();
+    resetGame();
+  }
 }
 
 //used in playRound() to show a message when the player wins the round. Adds a point to the player score and deducts the gamescore by 1
-function playerWinsRound(){
-	const winMessage = 'Player scored a point!';
-	console.info(winMessage);
-		++playerScore
-		--gameScore
+function playerWinsRound() {
+  const winMessage = "Player scored a point!";
+  console.info(winMessage);
+  ++playerScore;
+  --gameScore;
 }
 
 //used in playRound() to show a message when the player loses the round. Adds a point to the computerScore and deducts the gamescore by 1
-function playerLosesRound(){
-	const loseMessage = 'Computer scored a point!';
-	console.info(loseMessage);
-		++computerScore
-		--gameScore
+function playerLosesRound() {
+  const loseMessage = "Computer scored a point!";
+  console.info(loseMessage);
+  ++computerScore;
+  --gameScore;
 }
 //used in playRound() to display a message when it's a tie. Deducts the gamescore by 1.
-function roundIsaTie(){
-	const tieMessage = "The round was a tie!";
-	console.warn(tieMessage);
-	--gameScore
+function roundIsaTie() {
+  const tieMessage = "The round was a tie!";
+  console.warn(tieMessage);
+  --gameScore;
 }
 
 /*write a function declaration called playRound() 
@@ -109,42 +108,50 @@ function roundIsaTie(){
 	If playerSelection is rock and computerSelection is Paper OR if playerSelection is paper and computerSelection is scissors 
 	OR if playerSelection is scissors and computerSelection is rock then output a message that the player lost.
 	Add a point to the computerScore and Subtract 1 from the gameScore variable and return.*/
-function playRound(){
-	const computer = computerSelection();
-	const player = playerInput();
-	const match = `Player chose ${player} and computer chose ${computer}.`;
-	
-	console.log(match);
-	
-	if (gameScore === 0 && (playerScore === computerScore)){
-		if (player === computer){
-			roundIsaTie();
-			showScore();
-			showRoundsLeft();
-		} else if (player === 'rock' && computer === 'scissors' || player === 'paper' && computer === 'rock' || player === 'scissors' && computer === 'paper'){
-			playerWinsRound();
-			showScore();
-			showRoundsLeft();
-		} else {
-			playerLosesRound();
-			showScore();
-			showRoundsLeft();
-		}
-	} else {
-			if (player === computer){
-				roundIsaTie();
-				showScore();
-				showRoundsLeft();
-			} else if (player === 'rock' && computer === 'scissors' || player === 'paper' && computer === 'rock' || player === 'scissors' && computer === 'paper'){
-				playerWinsRound();
-				showScore();
-				showRoundsLeft();
-			} else {
-				playerLosesRound();
-				showScore();
-				showRoundsLeft();
-		}
-	}
+function playRound() {
+  const computer = computerSelection();
+  const player = playerInput();
+  const match = `Player chose ${player} and computer chose ${computer}.`;
+
+  console.log(match);
+
+  if (gameScore === 0 && playerScore === computerScore) {
+    if (player === computer) {
+      roundIsaTie();
+      showScore();
+      showRoundsLeft();
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      playerWinsRound();
+      showScore();
+      showRoundsLeft();
+    } else {
+      playerLosesRound();
+      showScore();
+      showRoundsLeft();
+    }
+  } else {
+    if (player === computer) {
+      roundIsaTie();
+      showScore();
+      showRoundsLeft();
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      playerWinsRound();
+      showScore();
+      showRoundsLeft();
+    } else {
+      playerLosesRound();
+      showScore();
+      showRoundsLeft();
+    }
+  }
 }
 
 playRound();
